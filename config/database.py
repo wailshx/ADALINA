40c47e409f16a9d7b551a180b0dd8ece03e4f94f
wail <wailkr68@gmail.com>
2026-07-13 17:19:33 +0100

    Remove hardcoded database credentials from code

--- config/database.py
+++ config/database.py
@@ -2,10 +2,7 @@ import os
 import psycopg2
 import psycopg2.extras
 
-DATABASE_URL = os.environ.get(
-    'DATABASE_URL',
-    'postgresql://postgres.uqflrwcwhxzhrwuokomw:Wailkerma02.@aws-0-eu-west-1.pooler.supabase.com:6543/postgres'
-)
+DATABASE_URL = os.environ.get('DATABASE_URL', '')
 
 class _ConnectionWrapper:
     def __init__(self, conn):
